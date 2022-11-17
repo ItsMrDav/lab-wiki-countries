cc394a4b75b6385ea67971fbe376b46064c35e65
ItsMrDav <davutt.simsek@gmail.com>
2022-11-17 20:26:54 +0100

    gotta work on it again

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function Navbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-dark bg-primary mb-3">
+        <div className="container">
+          <a className="navbar-brand" href="/">
+            WikiCountries
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
